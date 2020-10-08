0948abe869b90c8b87b9ff8cbd2ab1e05f5843b4
Kim Nguyen <nnhk23@gmail.com>
2020-10-08 13:17:06 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
+  @@count = []
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    @@count = self.split.select do |sentence|
+      sentence.sentence? || sentence.question? || sentence.exclamation? == true
+    end
+    @@count.count
   end
+  
+  
 end
+# greeting = String.new("This is a string! It has three sentences. Right?")
+# greeting.count_sentences
+# binding.pry
